52dea5453f0075600c1c136a00cc7e49cf4fb72a
fahadbinalam126 <fahad24hr@gmail.com>
2019-12-29 23:50:07 -0800

    First Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDRPXOq0SCnYubCjQ-jYAXVQ4QmaAWj8TU",
+    authDomain: "myphotoapptest-8c57b.firebaseapp.com",
+    databaseURL: "https://myphotoapptest-8c57b.firebaseio.com",
+    projectId: "myphotoapptest-8c57b",
+    storageBucket: "myphotoapptest-8c57b.appspot.com",
+    messagingSenderId: "192714066476",
+    appId: "1:192714066476:web:795ab631630ba003fadf89"
+  }
 };
 
 /*
